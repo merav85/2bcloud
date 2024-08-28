a82d606120724ce1fc11165e016a4182b96de2af
merav85 <merav85@gmail.com>
2024-08-28 14:13:22 +0300

    update virtual machines creation subnets automation

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
   skip_provider_registration = true
 }
 
-data "azurerm_resource_group" "main" {
+data "azurerm_resource_group" "example" {
   name     = "Merav-Candidate"
 }
 
@@ -27,57 +27,129 @@ cat << EOF > /etc/customdata
 example data
 EOF
 CUSTOM_DATA
+  virtual_machine_name = "${var.prefix}-vm"
 }
 
-resource "azurerm_virtual_network" "main" {
+resource "azurerm_virtual_network" "example" {
   name                = "${var.prefix}-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
+  address_space       = ["172.16.0.0/16"]
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+}
+
+resource "azurerm_subnet" "external" {
+  name                 = "external"
+  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = data.azurerm_resource_group.example.name
+  address_prefixes     = ["172.16.1.0/24"]
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = data.azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = data.azurerm_resource_group.example.name
+  address_prefixes     = ["172.16.2.0/24"]
 }
 
-resource "azurerm_network_interface" "main" {
-  name                = "${var.prefix}-nic"
-  resource_group_name = data.azurerm_resource_group.main.name
-  location            = data.azurerm_resource_group.main.location
+resource "azurerm_public_ip" "example" {
+  name                = "${var.prefix}-pip"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "${var.prefix}-nsg"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "allow_SSH"
+    description                = "Allow SSH access"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow_RDP"
+    description                = "Allow RDP access"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "external" {
+  name                      = "${var.prefix}-ext-nic"
+  location                  = data.azurerm_resource_group.example.location
+  resource_group_name       = data.azurerm_resource_group.example.name
 
   ip_configuration {
-    name                          = "internal"
+    name                          = "primary"
+    subnet_id                     = azurerm_subnet.external.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "external" {
+  network_interface_id      = azurerm_network_interface.external.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
+resource "azurerm_network_interface" "internal" {
+  name                = "${var.prefix}-int-nic"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "primary"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_linux_virtual_machine" "main" {
-  name                            = "${var.prefix}-vm"
-  resource_group_name             = data.azurerm_resource_group.main.name
-  location                        = data.azurerm_resource_group.main.location
-  size                            = "Standard_F2"
-  admin_username                  = "adminuser"
-  admin_password                  = "P@ssw0rd1234!"
-  custom_data                     = base64encode(local.custom_data)
-  disable_password_authentication = false
-  network_interface_ids = [
-    azurerm_network_interface.main.id,
-  ]
+resource "azurerm_virtual_machine" "main" {
+  name                          = "${var.prefix}-vm"
+  location                      = data.azurerm_resource_group.example.location
+  resource_group_name           = data.azurerm_resource_group.example.name
+  primary_network_interface_id  = azurerm_network_interface.external.id
+  network_interface_ids         = ["${azurerm_network_interface.external.id}", "${azurerm_network_interface.internal.id}"]
+  vm_size                       = "Standard_DS1_v2"
+  delete_os_disk_on_termination = true
 
-
-  source_image_reference {
+  storage_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
     sku       = "22_04-lts"
     version   = "latest"
   }
 
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
+  storage_os_disk {
+    name              = "${local.virtual_machine_name}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = local.virtual_machine_name
+    admin_username = "myadmin"
+    admin_password = "Passwword1234"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
   }
 }
